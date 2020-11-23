fd4dca2043ea058b3c153ab4ae01f66eb099d89f
Valdo <vlorian22@GMAIL.COM>
2020-11-23 17:59:49 -0600

    9.5.2 module

--- src/page-template.js
+++ src/page-template.js
@@ -3,7 +3,7 @@
 //if it doesn't exist, we'll simply return an empty string. If it does exist, however,
  //we'll return the entire <section> element. Let's update the main function's template
  // literal to execute generateAbout() where we want the content to go.
-const generabout = (aboutText) => {
+const generateAbout = (aboutText) => {
   if (!aboutText) {
     return "";
   }
@@ -12,6 +12,53 @@ const generabout = (aboutText) => {
     <p>${aboutText}
     </section>`;
 };
+const generateProjects= projectArr =>{
+
+    const generateProjects = projectsArr => {
+        return `
+          <section class="my-3" id="portfolio">
+            <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
+            <div class="flex-row justify-space-between">
+            ${projectsArr
+              .filter(({ feature }) => feature)
+              .map(({ name, description, languages, link }) => {
+                return `
+                <div class="col-12 mb-2 bg-dark text-light p-3">
+                  <h3 class="portfolio-item-title text-light">${name}</h3>
+                  <h5 class="portfolio-languages">
+                    Built With:
+                    ${languages.join(', ')}
+                  </h5>
+                  <p>${description}</p>
+                  <a href="${link}" class="btn"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+                </div>
+              `;
+              })
+              .join('')}
+      
+            ${projectsArr
+              .filter(({ feature }) => !feature)
+              .map(({ name, description, languages, link }) => {
+                return `
+                <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
+                  <h3 class="portfolio-item-title text-light">${name}</h3>
+                  <h5 class="portfolio-languages">
+                    Built With:
+                    ${languages.join(', ')}
+                  </h5>
+                  <p>${description}</p>
+                  <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+                </div>
+              `;
+              })
+              .join('')}
+            </div>
+          </section>
+        `;
+      };
+};
+    
+
 module.exports = (templateData) => {
   // destructure page data by section
   const { projects, about, ...header } = templateData;
@@ -45,6 +92,7 @@ module.exports = (templateData) => {
       </header>
       <main class="container my-5">
       ${generateAbout(about)}
+      ${generateProjects(projects)}
       </main>
       <footer class="container text-center py-3">
         <h3 class="text-dark">&copy; ${new Date().getFullYear()} by ${
