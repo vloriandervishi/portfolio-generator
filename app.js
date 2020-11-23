5100c7645fde2af4046c3fea4f2d38fef300315a
Valdo <vlorian22@GMAIL.COM>
2020-11-23 12:07:25 -0600

    added when to confirm about

--- app.js
+++ app.js
@@ -24,7 +24,14 @@ return inquirer .prompt([
   {
     type:'input',
     name:'about', 
-    message: 'Provide some information about yourself:'
+    message: 'Provide some information about yourself:',
+    when: ({confirmAbout})=>{
+      if(confirmAbout){
+        return true;
+      }else {
+        return false;
+      }
+    }
   }
 ]);
 };
