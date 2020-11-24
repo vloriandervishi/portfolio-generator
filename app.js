ddf7cd577a097d8f4dc4f5e59fdc9e7c6f3a8893
Valdo <vlorian22@GMAIL.COM>
2020-11-23 18:12:10 -0600

    added copyfile and css file to src and dist folder index.html

--- app.js
+++ app.js
@@ -113,9 +113,20 @@ promptUser()
 .then(promptProject)
 .then(portfolioData=> {
   const pageHTML= generatePage(portfolioData);
-  fs.writeFile('./index.html', pageHTML,err=>{
-    if(err) throw new (err);
+  fs.writeFile('./dist/index.html', pageHTML,err=>{
+    if(err){
+      console.log(err);
+      return;
+    }
+    
   });
   console.log('Page created! checkout index.html in this directory to see it' );
+  fs.copyFile('./src/style.css','./dist/style.css',err=>{
+    if(err){
+      console.log(err);
+      return;
+    }
+    console.log('Style sheet copied successfully!');
+  });
 });
 
